15667ccbafac6c89769459ccf90452ff837afe9b
Felix Fernandez <fernandezfelix460.gmail.com>
2019-12-08 12:32:19 -0500

    created syntax for html

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
+  selectAll: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: function(whatToSelect, table, orderCol) {
+    var queryString = "INSERT ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+
+  updateOne: function(whatToSelect, table, orderCol) {
+    var queryString = "UPDATE ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+ 
+};
+
+module.exports = orm;
